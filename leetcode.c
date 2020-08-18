2631b234797b8dd4bd2d9660e2d1b10f865c3de6
wei J <1305517085@qq.com>
2020-08-18 18:55:25 +0800

    bit

--- leetcode.c
+++ leetcode.c
@@ -2,7 +2,7 @@
 //
 
 #define _CRT_SECURE_NO_WARNINGS
- 
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
@@ -10,24 +10,61 @@
 
 void test01() {
 
-    Dynamic_Array* myArray = Init_Array();
-    for (int i = 0; i < 30; i++) {
-        PushBack_Array(myArray, i);
-    }
-    PrintArray(myArray);
+	Dynamic_Array* myArray = Init_Array();
+	for (int i = 0; i < 30; i++) {
+		PushBack_Array(myArray, i);
+	}
+	PrintArray(myArray);
 
-    RemoveByPos_Array(myArray, 0);
-    RemoveByValue_Array(myArray, 19);
+	RemoveByPos_Array(myArray, 0);
+	RemoveByValue_Array(myArray, 19);
 
-    PrintArray(myArray);
-    Find_Array(myArray, 6);
+	PrintArray(myArray);
+	Find_Array(myArray, 6);
 
-    //delete
-    FreeSpace_Array(myArray);
+	//delete
+	FreeSpace_Array(myArray);
 }
 int main()
 {
+	unsigned char buff[4];
+	unsigned char buff_out[4];
 
+	unsigned int bit_array;
+	buff[0] = 0x01;
+	buff[1] = 0x00;
+	buff[2] = 0x00;
+	buff[3] = 0x00;
+
+	bit_array = 0;
+	bit_array |= buff[0];
+	bit_array |= buff[1] << 8;
+	bit_array |= buff[2] << 16;
+	bit_array |= buff[3] << 24;
+	unsigned char* temp;
+	temp = (unsigned char*)&bit_array;
+	//unsigned int mask = 0xffffff00;
+	//memcpy(buff, &bit_array, 4*sizeof(unsigned char));
+	//memcpy(&bit_array, buff_out, 4*sizeof(unsigned char));
+	//buff_out[0] =  bit_array & 0xff;
+	//buff_out[1] = (bit_array>>8) & 0xff;
+	//buff_out[2] = (bit_array >> 16) & 0xff;
+	//buff_out[3] = (bit_array >> 24) & 0xff;
+	//buff_out[0] = *(temp + 0);
+	//buff_out[1] = *(temp + 1);
+	//buff_out[2] = *(temp + 2);
+	//buff_out[3] = *(temp + 3);
+
+
+	int a = 8;
+	int n = a % 16;
+
+	
+
+	while (1)
+	{
+		
+	}
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
