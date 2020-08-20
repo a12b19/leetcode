517efebfe9af3baa46ec9472ff21b0cadf9b2c87
wei J <1305517085@qq.com>
2020-08-20 20:01:59 +0800

    template

--- DynamicArray.c
+++ DynamicArray.c
@@ -1,120 +1,42 @@
 #include "DynamicArray.h"
 #include<stdio.h>
-Dynamic_Array* Init_Array() {
 
-	Dynamic_Array* myArray = (Dynamic_Array*)malloc(sizeof(Dynamic_Array));
-	myArray->size = 0;
-	myArray->capacity = 20;//default 
-	myArray->pAddr = (int*)malloc(sizeof(int) * myArray->capacity);
-	
-	return myArray;
-}
-
-//insert
-void PushBack_Array(Dynamic_Array* arr, int value) {
-	if (arr == NULL) {
-		return;
-	}
-	//judege space remain
-	if (arr->size == arr->capacity) {
-		//expand space
-		//1:new space is double old
-		int* newSpace = (int*)malloc(sizeof(int) * arr->capacity *2);
-		//2:copy data
-		memcpy(newSpace, arr->pAddr, arr->capacity * sizeof(int));
-		//3:free old space
-		free(arr->pAddr);
-		//upadte capacity
-		arr->capacity = arr->capacity * 2;
-		arr->pAddr = newSpace;
-
-	}
-	//insert new element
-	arr->pAddr[arr->size] = value;
-	arr->size++;
-
-}
-//delete by pos //by value
-void RemoveByPos_Array(Dynamic_Array* arr, int pos) {
-	if (arr == NULL) {
-		return;
-	}
-	if (pos < 0 || pos >= arr->size)return;
-	for (int i = pos; i < arr->size; i++) { //question  here
-		arr->pAddr[i] = arr->pAddr[i + 1];
-	}
-	arr->size--;
-
-}
-void RemoveByValue_Array(Dynamic_Array* arr, int value) {
-	if (arr == NULL)return;
-	int pos = Find_Array(arr, value);
-	RemoveByPos_Array(arr, pos);
-}
-
-//find
-int Find_Array(Dynamic_Array* arr, int value) {
-	if (arr == NULL)return -1;
-	int pos = -1;
-		for (int i = 0; i < arr->size; i++) {
-			if (arr->pAddr[i] == value) {
-				pos = i;
-				break;
-			}
-		}
-	return pos;
-
-
-}
-//print
-void PrintArray(Dynamic_Array* arr) {
-
-	if (arr == NULL) {
-		return;
-	}
-	for (int i = 0; i < arr->size; i++) {
-		printf("[%d]==%d ", i,arr->pAddr[i]);
-
-	}
-	printf("\n");
-
-}
-//freespace
-void FreeSpace_Array(Dynamic_Array* arr) {
-	if (arr == NULL) {
-		return;
-	}
-	if (arr->pAddr != NULL) {
-		free(arr->pAddr);
-	}
+ListNode* InitList() {
+	ListNode* list = (ListNode*)malloc(sizeof(ListNode));
+	list->next = NULL;
+	return list;
 
 
 }
 
-//clear array
-void Clear_Array(Dynamic_Array* arr) {
-	if (arr == NULL) {
-		return;
+void InsertList(ListNode* list, int val) {
+
+	while (list->next != NULL)
+	{
+		list = list->next;
+
+
 	}
-	arr->size = 0;
+	list->val = val;
+	ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+	list->next = newNode;
+	newNode->val = 0;
+	newNode->next = NULL;
 
 }
-//get capacity
-int Capacity_Array(Dynamic_Array* arr) {
-	if (arr == NULL) {
-		return;
-	}
-	return arr->capacity;
-}
-//get size
-int Size_Array(Dynamic_Array* arr) {
-	if (arr == NULL) {
-		return;
-	}
-	return arr->size;
-}
-//fet value of array
-int At_Array(Dynamic_Array* arr, int pos) {
 
-	return arr->pAddr[pos];
+void PrintfList(ListNode* list) {
+	do
+	{
+		printf("==%d\n", list->val);
+		list = list->next;
+
+
+	} while (list->next != NULL);
+
+
+
+
 }
+
+
--- leetcode.c
+++ leetcode.c
@@ -2,56 +2,19 @@
 //
 
 #define _CRT_SECURE_NO_WARNINGS
- 
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 #include "DynamicArray.h"
 
-void test01() {
 
-    Dynamic_Array* myArray = Init_Array();
-    for (int i = 0; i < 30; i++) {
-        PushBack_Array(myArray, i);
-    }
-    PrintArray(myArray);
 
-    RemoveByPos_Array(myArray, 0);
-    RemoveByValue_Array(myArray, 19);
-
-    PrintArray(myArray);
-    Find_Array(myArray, 6);
-
-    //delete
-    FreeSpace_Array(myArray);
-}
 int main()
 {
-    const char str[] = "http://www[runoob]com";
-    const char ch = '[';
-    char* ret;
-
-    ret = strchr(str, ch);
-
-    printf("|%c| 之后的字符串是 - |%s|\n", ch, ret);
-    const char tag_name[] = "Program:MainProgram.datagrop1[439]";
 
 
-    char* open_bracket = str;
-    char* close_bracket = str;
-    open_bracket = strchr(tag_name, '[');
-    close_bracket = strchr(tag_name, ']');
-    printf("\t\t\t\t\[==%s   ]==%s\n", open_bracket, close_bracket);
-    char index_str[10];
-    strcpy(index_str, open_bracket+1);
-    int i;
-    for (i = 0; index_str[i] != ']'; i++);
-    index_str[i] = '\0';
-    int index = atoi(index_str);
-    char* index_temp;
-    index_temp = open_bracket + 1;
-    printf("indedx==%s\n", index_temp);
-    //test01();
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
